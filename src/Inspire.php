8d8c7cf63a689418fccf9729b6db85689a49530d
k_tith <tithkhem@kesspay.io>
2024-06-04 14:34:21 +0700

    fixed name

--- src/Inspire.php
+++ src/Inspire.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class Inspire {
     public function justDoIt() {
+        dd("just do it");
         $response = Http::get('https://inspiration.goprogram.ai/');
         return $response['quote'] . ' -' . $response['author'];
     }
